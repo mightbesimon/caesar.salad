426971164329d40e231a48256bbcd38a64c5ac4f
mightbesimon <60593422+mightbesimon@users.noreply.github.com>
2021-08-01 19:44:04 +1200

    added salt and pepper

--- examples.py
+++ examples.py
@@ -12,7 +12,7 @@ ciphertext3 = 'FRZDUGV GLH PDQB WLPHV EHIRUH WKHLU GHDWKV; WKH YDOLDQW QHYHU WDV
 ciphertext4 = 'Z. VKDNHVSHDUH, MXOLXV FDHVDU'
 #             'w. shakespeare, julius caesar'
 ciphertext5 = 'BUUBDL BU EBXO'
-#              'attack at dawn'
+#             'attack at dawn'
 ciphertext6 = 'KYV MVIP NFIU “JVTIVTP” ZJ IVGLXEREK ZE R WIVV REU FGVE JFTZVKP; REU NV RIV RJ R GVFGCV ZEYVIVEKCP REU YZJKFIZTRCCP FGGFJVU KF JVTIVK JFTZVKZVJ, KF JVTIVK FRKYJ REU KF JVTIVK GIFTVVUZEXJ. NV UVTZUVU CFEX RXF KYRK KYV UREXVIJ FW VOTVJJZMV REU LENRIIREKVU TFETVRCDVEK FW GVIKZEVEK WRTKJ WRI FLKNVZXYVU KYV UREXVIJ NYZTY RIV TZKVU KF ALJKZWP ZK. AFYE W BVEEVUP'
 #             'the very word “secrecy” is repugnant in a free and open society; and we are as a people inherently and historically opposed to secret societies, to secret oaths and to secret proceedings. we decided long ago that the dangers of excessive and unwarranted concealment of pertinent facts far outweighed the dangers which are cited to justify it. john f kennedy'
 
